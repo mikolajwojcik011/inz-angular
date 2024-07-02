416dd2cd066f07ff54ec9ca7fa7156b8d83c1527
Mikołaj <mikolajwojcik011@gmail.com>
2024-07-02 12:25:03 +0200

    store refactor finished

--- src/app/store/test.selectors.ts
+++ src/app/store/test.selectors.ts
@@ -6,12 +6,10 @@ interface AppState {
   test: TestState;
 }
 
-export const selectTest = createFeatureSelector<any>('test');
+export const selectTestState = createFeatureSelector<AppState>('test');
 
-// export const selectTestState = (state: AppState) => state.test;
-//
-// export const selectTest = createSelector(
-//   selectTestState,
-//   (state: TestState) => state.test
-// );
+export const selectTest = createSelector(
+  selectTestState,
+  (state: AppState) => state.test
+);
 
