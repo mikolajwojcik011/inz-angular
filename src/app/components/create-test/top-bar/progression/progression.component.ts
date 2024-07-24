496c0865cfb042e95ac3f0c8aa66c8fde495643a
Mikołaj <mikolajwojcik011@gmail.com>
2024-07-25 00:19:35 +0200

    top bar ui finished

---
+++ src/app/components/create-test/top-bar/progression/progression.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-progression',
+  standalone: true,
+  imports: [],
+  templateUrl: './progression.component.html',
+  styleUrl: './progression.component.css'
+})
+export class ProgressionComponent {
+  @Input() done: number = 0;
+  @Input() total: number = 0;
+}
