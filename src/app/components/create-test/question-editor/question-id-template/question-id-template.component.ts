b290245b69a79518f415bb71958bf7a46242cfd3
Mikołaj <mikolajwojcik011@gmail.com>
2024-07-26 19:56:08 +0200

    fixes

--- src/app/components/create-test/question-editor/question-id-template/question-id-template.component.ts
+++ src/app/components/create-test/question-editor/question-id-template/question-id-template.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ButtonAddQuestionComponent} from "../../../shared/buttons/button-add-question/button-add-question.component";
 
 @Component({
   selector: 'app-question-id-template',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonAddQuestionComponent
+  ],
   templateUrl: './question-id-template.component.html',
   styleUrl: './question-id-template.component.css'
 })
