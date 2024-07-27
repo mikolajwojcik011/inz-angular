81ea08ca9c640ac2fb37c9836fdc44f4890095b7
Mikołaj <mikolajwojcik011@gmail.com>
2024-07-27 17:45:12 +0200

    question editor progress

--- src/app/components/create-test/question-editor/question-id-template/question-id-template.component.ts
+++ src/app/components/create-test/question-editor/question-id-template/question-id-template.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {ButtonAddQuestionComponent} from "../../../shared/buttons/button-add-question/button-add-question.component";
 import {InputTemplateTextComponent} from "../../../shared/inputs/input-template-text/input-template-text.component";
 import {SelectTemplateComponent} from "../../../shared/inputs/select-template/select-template.component";
+import {TextAreaTemplateComponent} from "../../../shared/inputs/text-area-template/text-area-template.component";
 
 @Component({
   selector: 'app-question-id-template',
@@ -9,7 +10,8 @@ import {SelectTemplateComponent} from "../../../shared/inputs/select-template/se
   imports: [
     ButtonAddQuestionComponent,
     InputTemplateTextComponent,
-    SelectTemplateComponent
+    SelectTemplateComponent,
+    TextAreaTemplateComponent
   ],
   templateUrl: './question-id-template.component.html',
   styleUrl: './question-id-template.component.css'
